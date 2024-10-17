ed830d919b076a7aaec9679f82323f070e357bbc
Nahail Ahmad <ahmadnahail08@gmail.com>
2024-10-17 14:01:36 +0500

    New changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,7 @@ import FAQComponent from '@/components/faq-component';
 
 export default function Home() {
   return (
-    <main className="min-h-screen p-24">
-      <h1 className="text-4xl font-bold mb-8">Welcome to Our Platform</h1>
+    <main className="min-h-screen p-24">    
       <FAQComponent />
     </main>
   )
